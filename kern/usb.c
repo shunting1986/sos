2a3f19eeca88bcc0b26709fbe7225db8d0a3ff07
shunting <zhangst1986@gmail.com>
2017-02-01 08:37:38 +0800

    validate universal host controller class code

--- kern/usb.c
+++ kern/usb.c
@@ -2,8 +2,20 @@
 #include <kern/printf.h>
 #include <kern/pci.h>
 #include <kern/assert.h>
+#include <inc/stdint.h>
 
 static int usb_iobase;
+
+static void check_classcode(int bus, int dev, int func) {
+	outl(PCI_CONF_ADDR, PACK_ADDR(bus, dev, func, 2));
+	uint32_t data = inl(PCI_CONF_DATA);
+	int classcode = (data >> 24) & 0xff;
+	int subcc = (data >> 16) & 0xff;
+	int progif = (data >> 8) & 0xff;
+	assert(classcode == 0x0C); // serial bus controller
+	assert(subcc == 0x03); // usb host controller
+	assert(progif == 0); // undef
+}
 
 void usb_controller_init(int bus, int dev, int func) {
 	// need bar4
@@ -25,5 +37,7 @@ void usb_controller_init(int bus, int dev, int func) {
 	printf("usb_iobase %x size %d\n", usb_iobase, ioreg_size);
 	assert((ioreg_size & (-ioreg_size)) == ioreg_size);
 	assert(usb_iobase % ioreg_size == 0);
+
+	check_classcode(bus, dev, func);
 }
 
